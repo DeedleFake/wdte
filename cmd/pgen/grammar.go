69de6b213f166ca2d9016136afd3b4e779b16a22
DeedleFake <deedlefake@users.noreply.github.com>
2017-04-21 14:31:59 -0400

    cmd/pgen: Still working on basic implementation.

--- cmd/pgen/grammar.go
+++ cmd/pgen/grammar.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"io"
+	"strings"
 
 	"github.com/DeedleFake/wdte/scanner"
 )
@@ -10,18 +12,40 @@ type Grammar struct {
 	NTerms map[string]NTerm
 }
 
-func LoadGrammar(r io.Reader) (Grammar, error) {
+func LoadGrammar(r io.Reader) (g Grammar, err error) {
+	g = Grammar{
+		NTerms: make(map[string]NTerm),
+	}
+
+	var cur NTerm
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		line := s.Text()
+
+		parts := strings.SplitN(line, "->", 2)
+		if len(parts) < 2 {
+			parts = strings.SplitN(line, "|", 2)
+			parts[0] = cur.Name
+		}
+		name := strings.TrimSpace(parts[0])
+		if nt, ok := g.NTerms[name]; ok {
+			cur = nt
+		}
+		cur.Name = name
+	}
+	if s.Err() != nil {
+		return g, s.Err()
+	}
+
 	panic("Not implemented.")
 }
 
-type Term struct {
-	scanner.Token
-}
+type Term scanner.TokenType
 
 type NTerm struct {
 	Name   string
-	First  map[string]struct{}
-	Follow map[string]struct{}
+	First  map[scanner.TokenType]struct{}
+	Follow map[scanner.TokenType]struct{}
 }
 
 type Epsilon struct{}
