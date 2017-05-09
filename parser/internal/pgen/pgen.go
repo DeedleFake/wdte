9f651de7a6d0931541d65866302275daf19eaf7f
DeedleFake <deedlefake@users.noreply.github.com>
2017-05-09 00:00:59 -0400

    parser/internal/pgen: Generate.

---
+++ parser/internal/pgen/pgen.go
@@ -0,0 +1,52 @@
+package pgen
+
+import "github.com/DeedleFake/wdte/scanner"
+
+//go:generate pgen -out table.go ../../../res/grammar.ebnf
+
+type Term struct {
+	Type    scanner.TokenType
+	Keyword string
+}
+
+func newTerm(t string) Term {
+	switch t {
+	case "id":
+		return Term{
+			Type: scanner.ID,
+		}
+
+	case "string":
+		return Term{
+			Type: scanner.String,
+		}
+
+	case "number":
+		return Term{
+			Type: scanner.Number,
+		}
+	}
+
+	return Term{
+		Type:    scanner.Keyword,
+		Keyword: t,
+	}
+}
+
+type NTerm string
+
+func newNTerm(nt string) NTerm {
+	return NTerm(nt)
+}
+
+type Epsilon struct{}
+
+func newEpsilon() Epsilon {
+	return Epsilon{}
+}
+
+func newRule(tokens ...Token) (r Rule) {
+	r = make(Rule, len(tokens))
+	copy(r, tokens)
+	return
+}
