a1484361b036687eb8d445dadad8d0e75f089c67
DeedleFake <deedlefake@users.noreply.github.com>
2017-05-10 15:55:29 -0400

    std/stream: Change the way a mapper works. Fix some linter warnings.

--- std/stream/stream.go
+++ std/stream/stream.go
@@ -4,25 +4,48 @@ package stream
 
 import "github.com/DeedleFake/wdte"
 
+// A Stream is a type of function that can yield successive values.
 type Stream interface {
 	wdte.Func
 
+	// Next returns the next value and true, or an undefined value and
+	// false if the stream is empty.
 	Next(frame []wdte.Func) (wdte.Func, bool)
 }
 
+// A NextFunc wraps a Go function, making it possible to use it as a
+// Stream. When called as a WDTE function, the function simply returns itself.
+type NextFunc func(frame []wdte.Func) (wdte.Func, bool)
+
+func (n NextFunc) Call(frame []wdte.Func, args ...wdte.Func) wdte.Func {
+	return n
+}
+
+func (n NextFunc) Equals(other wdte.Func) bool {
+	panic("Not implemented.")
+}
+
+func (n NextFunc) Next(frame []wdte.Func) (wdte.Func, bool) {
+	return n(frame)
+}
+
+// An array is a Stream that iterates over an array.
 type array struct {
 	a wdte.Array
 	i int
 }
 
+// New returns a new stream. If given one argument and that argument
+// is an array, it iterates over the values of the array. If given
+// more than one argument or the first argument is not an array, it
+// iterates over its arguments.
 func New(frame []wdte.Func, args ...wdte.Func) wdte.Func {
 	switch len(args) {
 	case 0:
 		return wdte.GoFunc(New)
 
 	case 1:
-		a1 := args[0].Call(frame)
-		if a1, ok := a1.(wdte.Array); ok {
+		if a1, ok := args[0].Call(frame).(wdte.Array); ok {
 			return &array{a: a1}
 		}
 	}
@@ -48,12 +71,24 @@ func (a *array) Next(frame []wdte.Func) (wdte.Func, bool) {
 	return r, true
 }
 
+// An rng (range) is a stream that yields successive numbers.
 type rng struct {
+	// i is the next number to return.
 	i wdte.Number
+
+	// m is the number to stop at, exclusive.
 	m wdte.Number
+
+	// s is the amount to increment every time Next() is called.
 	s wdte.Number
 }
 
+// Range returns a stream that yields successive numbers. If given a
+// single argument, the range yielded is [0, args[0]). If given two
+// arguments, the range is [args[0], args[1]). If given three
+// arguments, the range is the same as with two arguments, but the
+// step in between numbers yielded is the third argument, rather than
+// 1.
 func Range(frame []wdte.Func, args ...wdte.Func) wdte.Func {
 	switch len(args) {
 	case 0:
@@ -98,11 +133,14 @@ func (r *rng) Next(frame []wdte.Func) (wdte.Func, bool) {
 	return n, true
 }
 
+// A mapper is a function that creates Stream wrappers that call a
+// function on each value yielded by the underlying Stream, returning
+// the result.
 type mapper struct {
 	m wdte.Func
-	s Stream
 }
 
+// Map returns a Stream which
 func Map(frame []wdte.Func, args ...wdte.Func) wdte.Func {
 	switch len(args) {
 	case 0:
@@ -116,7 +154,14 @@ func (m *mapper) Call(frame []wdte.Func, args ...wdte.Func) wdte.Func {
 	switch len(args) {
 	case 1:
 		if a, ok := args[0].Call(frame).(Stream); ok {
-			m.s = a
+			return NextFunc(func(frame []wdte.Func) (wdte.Func, bool) {
+				n, ok := a.Next(frame)
+				if !ok {
+					return nil, false
+				}
+
+				return m.m.Call(frame, n), true
+			})
 		}
 	}
 
@@ -127,19 +172,7 @@ func (m *mapper) Equals(other wdte.Func) bool {
 	panic("Not implemented.")
 }
 
-func (m *mapper) Next(frame []wdte.Func) (wdte.Func, bool) {
-	if m.s == nil {
-		return nil, false
-	}
-
-	n, ok := m.s.Next(frame)
-	if !ok {
-		return nil, false
-	}
-
-	return m.m.Call(frame, n), true
-}
-
+// Collect converts a Stream into an array.
 func Collect(frame []wdte.Func, args ...wdte.Func) wdte.Func {
 	switch len(args) {
 	case 0:
@@ -164,6 +197,9 @@ func Collect(frame []wdte.Func, args ...wdte.Func) wdte.Func {
 	return r
 }
 
+// Module returns a module for easy importing into an actual script.
+// The imported functions have the same names as the functions in this
+// package, except that the first letter is lowercase.
 func Module() *wdte.Module {
 	return &wdte.Module{
 		Funcs: map[wdte.ID]wdte.Func{
