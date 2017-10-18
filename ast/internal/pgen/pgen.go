73680c4b99518b6c110516dc650be22db7de68d7
DeedleFake <deedlefake@users.noreply.github.com>
2017-10-17 22:43:12 -0400

    ast/internal/pgen: Split into two packages, one for each grammar.
    Does this count as an abuse of type aliases?

--- ast/internal/pgen/pgen.go
+++ ast/internal/pgen/pgen.go
@@ -2,7 +2,6 @@ package pgen
 
 import "github.com/DeedleFake/wdte/scanner"
 
-//go:generate pgen -out table.go ../../../res/grammar.cfg
 type Token interface{}
 
 type Term struct {
@@ -18,41 +17,9 @@ func (t Term) String() string {
 	return t.Type.String()
 }
 
-func newTerm(t string) Term {
-	switch t {
-	case "id":
-		return Term{
-			Type: scanner.ID,
-		}
-
-	case "string":
-		return Term{
-			Type: scanner.String,
-		}
-
-	case "number":
-		return Term{
-			Type: scanner.Number,
-		}
-	}
-
-	return Term{
-		Type:    scanner.Keyword,
-		Keyword: t,
-	}
-}
-
 type NTerm string
 
-func newNTerm(nt string) NTerm {
-	return NTerm(nt)
-}
-
 type Epsilon struct{}
-
-func newEpsilon() Epsilon {
-	return Epsilon{}
-}
 
 func (e Epsilon) String() string {
 	return "ε"
@@ -64,10 +31,9 @@ func (e EOF) String() string {
 	return "Ω"
 }
 
-func newEOF() EOF {
-	return EOF{}
-}
+type Rule []Token
 
-func newRule(tokens ...Token) (r Rule) {
-	return Rule(tokens)
+type Lookup struct {
+	Term  Token
+	NTerm NTerm
 }
