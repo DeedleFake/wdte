3bc455ba24d552f3765369064862a9ead02ace0d
DeedleFake <deedlefake@users.noreply.github.com>
2017-10-21 22:07:10 -0400

    std/arrays: Add `stream`.

--- std/arrays/arrays.go
+++ std/arrays/arrays.go
@@ -33,10 +33,47 @@ func At(frame wdte.Frame, args ...wdte.Func) wdte.Func {
 	return a[int(i)]
 }
 
+// A streamer is a stream that iterates over an array.
+type streamer struct {
+	a wdte.Array
+	i int
+}
+
+// Stream returns a stream that iterates over a given array.
+func Stream(frame wdte.Frame, args ...wdte.Func) wdte.Func {
+	frame = frame.WithID("stream")
+
+	switch len(args) {
+	case 0:
+		return wdte.GoFunc(Stream)
+	}
+
+	return &streamer{a: args[0].Call(frame).(wdte.Array)}
+}
+
+func (a *streamer) Call(frame wdte.Frame, args ...wdte.Func) wdte.Func {
+	return a
+}
+
+func (a *streamer) Next(frame wdte.Frame) (wdte.Func, bool) {
+	if a.i >= len(a.a) {
+		return nil, false
+	}
+
+	r := a.a[a.i]
+	a.i++
+	return r, true
+}
+
+// Module returns a module for easy importing into an actual script.
+// The imported functions have the same names as the functions in this
+// package, except that the first letter is lowercase.
 func Module() *wdte.Module {
 	return &wdte.Module{
 		Funcs: map[wdte.ID]wdte.Func{
 			"at": wdte.GoFunc(At),
+
+			"stream": wdte.GoFunc(Stream),
 		},
 	}
 }
