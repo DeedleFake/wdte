d3e06d695f0e4713c69e8f74508490105a3c4ab7
DeedleFake <deedlefake@users.noreply.github.com>
2018-03-11 11:27:26 -0400

    wdte: Add a few more tests for let expressions.

--- wdte_test.go
+++ wdte_test.go
@@ -225,6 +225,21 @@ func TestBasics(t *testing.T) {
 			script: `main => (let x => 3; x);`,
 			ret:    wdte.Number(3),
 		},
+		{
+			name:   "Let/Args",
+			script: `main => (let add x y => + x y; add 3 5);`,
+			ret:    wdte.Number(8),
+		},
+		{
+			name:   "Let/Inner",
+			script: `main => (let x => 3; (let x => 5); x);`,
+			ret:    wdte.Number(3),
+		},
+		{
+			name:   "Let/Return",
+			script: `main => (let x => 3);`,
+			ret:    wdte.Number(3),
+		},
 	})
 }
 
