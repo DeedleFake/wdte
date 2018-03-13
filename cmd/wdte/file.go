c7593b6e96455de4b15429bcafcf5e1d30f5bda2
DeedleFake <deedlefake@users.noreply.github.com>
2018-03-12 21:49:26 -0400

    cmd/wdte: Implement file parsing.

--- cmd/wdte/file.go
+++ cmd/wdte/file.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"os"
 
 	"github.com/DeedleFake/wdte"
+	"github.com/DeedleFake/wdte/std"
 )
 
 func file(im wdte.Importer, file io.Reader) {
-	panic("Not implemented.")
+	m, err := wdte.Parse(file, im)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse script: %v", err)
+		os.Exit(1)
+	}
+
+	ret := m.Call(std.F())
+	if err, ok := ret.(error); ok {
+		fmt.Fprintf(os.Stderr, "Script returned an error: %v", err)
+		os.Exit(3)
+	}
 }
