c0b0033ed8903955f86715beffab709d5baa83bc
DeedleFake <deedlefake@users.noreply.github.com>
2018-10-25 22:45:03 -0400

    auto: Godoc.

--- auto/func.go
+++ auto/func.go
@@ -8,6 +8,32 @@ import (
 	"github.com/DeedleFake/wdte"
 )
 
+// Func returns a wdte.Func that wraps the given function f. The
+// returned function automatically translates any supported types to
+// and from the appropriate types for both arguments and return
+// values. The given name is used to name the frame used inside the
+// function.
+//
+// Note that currently this is limited to functions that have a single
+// return value, and not all types are supported. Currently supported
+// types are as follows:
+//
+// Arguments:
+// * Any number type, including uintptrs.
+// * Arrays and slices of supported types. Note that the passed WDTE
+//   array's length must match the expected length of the array in the
+//   Go function's arguments.
+// * Booleans.
+// * Strings.
+//
+// Return types:
+// * Any number type, including uintptrs.
+// * Arrays and slices of supported types.
+// * Booleans.
+// * Strings.
+// * Pointers to supported types.
+// * Functions that are supported by this function. The functions will
+//   use a frame with the name "<auto>".
 func Func(name string, f interface{}) wdte.Func {
 	v := reflect.ValueOf(f)
 
