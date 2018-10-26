58d1d7746d323db0c150e4bca815e1eb10077282
DeedleFake <deedlefake@users.noreply.github.com>
2018-10-25 22:29:31 -0400

    auto: Much better tests.

--- auto/func_test.go
+++ auto/func_test.go
@@ -11,10 +11,11 @@ import (
 
 func TestFunc(t *testing.T) {
 	tests := []struct {
-		name string
-		f    interface{}
-		args []wdte.Func
-		ret  wdte.Func
+		name  string
+		f     interface{}
+		args  []wdte.Func
+		ret   wdte.Func
+		calls []wdte.Func
 	}{
 		{
 			name: "Number",
@@ -24,15 +25,59 @@ func TestFunc(t *testing.T) {
 			args: []wdte.Func{wdte.Number(2)},
 			ret:  wdte.Number(5),
 		},
+		{
+			name: "Array",
+			f: func(a [3]int) [2]int {
+				return [...]int{a[0] + a[1], a[1] + a[2]}
+			},
+			args: []wdte.Func{wdte.Array{wdte.Number(1), wdte.Number(2), wdte.Number(3)}},
+			ret:  wdte.Array{wdte.Number(3), wdte.Number(5)},
+		},
+		{
+			name: "Slice",
+			f: func(s []int) []int {
+				var sum int
+				prod := 1
+				for _, v := range s {
+					sum += v
+					prod *= v
+				}
+				return []int{sum, prod}
+			},
+			args: []wdte.Func{wdte.Array{wdte.Number(1), wdte.Number(2)}},
+			ret:  wdte.Array{wdte.Number(3), wdte.Number(2)},
+		},
+		{
+			name: "Func",
+			f: func(v int) func() int {
+				return func() int {
+					v++
+					return v - 1
+				}
+			},
+			args:  []wdte.Func{wdte.Number(0)},
+			calls: []wdte.Func{wdte.Number(0), wdte.Number(1), wdte.Number(2)},
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			f := auto.Func(test.name, test.f)
 			r := f.Call(wdte.F(), test.args...)
-			if !reflect.DeepEqual(r, test.ret) {
+
+			if (test.ret != nil) && !reflect.DeepEqual(r, test.ret) {
 				t.Errorf("Got %#v", r)
 				t.Errorf("Expected %#v", test.ret)
+			}
+
+			if len(test.calls) > 0 {
+				for _, ret := range test.calls {
+					n := r.Call(wdte.F())
+					if !reflect.DeepEqual(n, ret) {
+						t.Errorf("Got %#v", n)
+						t.Errorf("Expected %#v", ret)
+					}
+				}
 			}
 		})
 	}
