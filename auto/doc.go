f5e1f5106018b39c6ae94c275cb7f7bc194e8c09
DeedleFake <deedlefake@users.noreply.github.com>
2018-10-30 15:16:12 -0400

    auto: Slight tweak to the package comment.

--- auto/doc.go
+++ auto/doc.go
@@ -1,3 +1,3 @@
-// Package auto provides higher-level automatic wrappers for Go/WDTE
-// interoperability.
+// Package auto provides higher-level automatic wrappers and
+// convenience for Go/WDTE interoperability.
 package auto
