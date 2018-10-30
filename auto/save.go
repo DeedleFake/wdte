feeb05419c7dc911810c11a372fa4bb2dde05e0a
DeedleFake <deedlefake@users.noreply.github.com>
2018-10-30 15:39:14 -0400

    auto: Add fast paths for saving with no arguments.

--- auto/save.go
+++ auto/save.go
@@ -5,6 +5,10 @@ import "github.com/DeedleFake/wdte"
 // SaveArgs returns a function which, when called, prepends args to
 // its argument list.
 func SaveArgs(f wdte.Func, args ...wdte.Func) wdte.Func {
+	if len(args) == 0 {
+		return f
+	}
+
 	return wdte.GoFunc(func(frame wdte.Frame, next ...wdte.Func) wdte.Func {
 		return f.Call(frame, append(args, next...)...)
 	})
@@ -13,6 +17,10 @@ func SaveArgs(f wdte.Func, args ...wdte.Func) wdte.Func {
 // SaveArgsReverse returns a function which, when called, appends args
 // to its argument list.
 func SaveArgsReverse(f wdte.Func, args ...wdte.Func) wdte.Func {
+	if len(args) == 0 {
+		return f
+	}
+
 	return wdte.GoFunc(func(frame wdte.Frame, next ...wdte.Func) wdte.Func {
 		return f.Call(frame, append(next, args...)...)
 	})
