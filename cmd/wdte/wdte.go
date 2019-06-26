2ea598e6b686db6032429f6457d1d686c2477fa8
DeedleFake <DeedleFake@users.noreply.github.com>
2019-06-26 15:37:48 -0400

    cmd/wdte: Add version info. (#185)

--- cmd/wdte/wdte.go
+++ cmd/wdte/wdte.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"runtime"
+	"runtime/debug"
 	"strings"
 )
+
+func wdteVersion() (string, error) {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "", errors.New("Failed to read build info")
+	}
+
+	for _, dep := range bi.Deps {
+		if dep.Path != "github.com/DeedleFake/wdte" {
+			continue
+		}
+
+		return dep.Version, nil
+	}
+
+	return bi.Main.Version, nil
+}
 
 func main() {
 	blacklist := flag.String("blacklist", "", "Comma-separated list of modules that can't be imported.")
 	eval := flag.String("e", "", "An expression to evaluate instead of reading from a file.")
+	version := flag.Bool("version", false, "Print the Go and WDTE versions and then exit.")
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %v [options] [<file> | -] [arguments...]\n\n", os.Args[0])
 
@@ -17,6 +38,19 @@ func main() {
 		flag.PrintDefaults()
 	}
 	flag.Parse()
+
+	if *version {
+		g := runtime.Version()
+		w, err := wdteVersion()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to get WDTE version: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Go: %v\n", g)
+		fmt.Printf("WDTE: %v\n", w)
+		return
+	}
 
 	im := importer("", strings.Split(*blacklist, ","), flag.Args(), nil)
 
