e1a8948f5a6f31afd14391e3776c18993d21d7ff
DeedleFake <deedlefake@users.noreply.github.com>
2019-06-26 15:38:34 -0400

    std/all: Generate list.
    I noticed that `rand` was missing, so...

---
+++ std/all/gen.go
@@ -0,0 +1,11 @@
+package all
+
+import (
+	_ "github.com/DeedleFake/wdte/std/arrays"
+	_ "github.com/DeedleFake/wdte/std/io"
+	_ "github.com/DeedleFake/wdte/std/io/file"
+	_ "github.com/DeedleFake/wdte/std/math"
+	_ "github.com/DeedleFake/wdte/std/rand"
+	_ "github.com/DeedleFake/wdte/std/stream"
+	_ "github.com/DeedleFake/wdte/std/strings"
+)
